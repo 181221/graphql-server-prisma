8a4e9f98ff3d9323812ddb4e521480e03cf26493
Peder Aalen <pederwaa@ifi.uio.no>
2020-04-12 23:11:28 +0200

    add authentication through auth0

--- src/utils.ts
+++ src/utils.ts
@@ -2,6 +2,7 @@ import { verify, sign } from "jsonwebtoken";
 import { AuthenticationError } from "apollo-server-core";
 import { Context } from "./resolvers/Context";
 import { APP_SECRET } from "./index";
+import jwksClient = require("jwks-rsa");
 
 export const authenticate = (context: Context) => {
   const Authorization = context.request.get("Authorization");
@@ -16,3 +17,29 @@ export const authenticate = (context: Context) => {
 export const signKey = ({ id }, claims, expiresIn = "3h") => {
   return sign({ userId: id, claims }, APP_SECRET, { expiresIn });
 };
+
+const client = jwksClient({
+  strictSsl: true, // Default value
+  jwksUri: `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`,
+});
+const getKey = (header, callback) => {
+  client.getSigningKey(header.kid, (err, key) => {
+    if (!err) callback(null, key.getPublicKey());
+    else callback(null, err.stack);
+  });
+};
+
+export const isUserLoggedIn = async (thaToken) => {
+  return new Promise(async (resolve, reject) => {
+    verify(thaToken, getKey, { algorithms: ["RS256"] }, (err, decoded) => {
+      if (err) return reject(err.message);
+      return resolve(decoded);
+    });
+  });
+};
+/*
+isUserLoggedIn()
+  .then((res) => {
+    console.log("resolved", res);
+  })
+  .catch((error) => console.log("error occured", error));*/
